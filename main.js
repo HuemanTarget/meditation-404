cf01f3e19fd71a5b8369435a905c553e1d904284
HuemanTarget <joshbasche@live.com>
2022-08-01 18:15:09 -0700

    add function to control play pause and animate timer

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const app = () => {
   const sounds = document.querySelectorAll('.sound-picker button');
   // Time Display
   const timeDisplay = document.querySelector('.time-display');
+  const timeSelect = document.querySelectorAll('.time-select button');
   // Outline Total Length
   const outlineLength = outline.getTotalLength();
   // Duration
@@ -16,9 +17,28 @@ const app = () => {
   outline.style.strokeDasharray = outlineLength;
   outline.style.strokeDashoffset = outlineLength;
 
+  // Pick Different Sounds
+  sounds.forEach((sound) => {
+    sound.addEventListener('click', function () {
+      song.src = this.getAttribute('data-sound');
+      video.src = this.getAttribute('data-video');
+      checkPlaying(song);
+    });
+  });
+
   // Play Sound
   play.addEventListener('click', () => {
     checkPlaying(song);
+  });
+
+  // Choose Song Duration
+  timeSelect.forEach((option) => {
+    option.addEventListener('click', function () {
+      fakeDuration = this.getAttribute('data-time');
+      timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+        fakeDuration % 60
+      )}`;
+    });
   });
 
   // Functions
@@ -33,6 +53,28 @@ const app = () => {
       song.pause();
       video.pause();
       play.src = './svg/play.svg';
+    }
+  };
+
+  // Animate Circle
+  song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    // Animate Circle
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+
+    // Animate Text
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+
+    if (currentTime >= fakeDuration) {
+      song.pause();
+      song.currentTime = 0;
+      play.src = './svg/play.svg';
+      video.pause();
     }
   };
 };
